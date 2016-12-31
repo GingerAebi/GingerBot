f21dfafa80001e53b67849c9dd7de998e139e98e
GingerAebi-MacAir <gingerAebi@gmail.com>
2016-12-31 16:21:05 +0900

    line library Change

--- reply.js
+++ reply.js
@@ -1,18 +1,38 @@
 var express = require('express');
 var router = express.Router();
 
-var lineBot = require('line-bot-sdk');
+var line = require('node-line-bot-api');
 var cfg = require('./config.json');
 
-var client = lineBot.client({cfg});
+line.init({
+    accessToken : cfg.channelToken,
+    channelSecret : cfg.channelSecret
+});
+
+var lineClient = line.client;
+var lineValidator = line.validator;
 
 router.get('/',function(req, res){
    res.json({"resCode" : 200, "resMsg" : "Success"}); 
 });
 
 router.post('/webhook',function (req, res) {
-    console.log('request : ' + req);
-    res.json({"resCode" : 200, "resMsg" : "Success"});
+    const promises = req.body.events.map(event => {
+    // reply message
+    return line.client
+      .replyMessage({
+        replyToken: event.replyToken,
+        messages: [
+          {
+            type: 'text',
+            text: event.message.text
+          }
+        ]
+      })
+  })
+  Promise
+    .all(promises)
+    .then(() => res.json({success: true}))
 });
 
 module.exports = router;
